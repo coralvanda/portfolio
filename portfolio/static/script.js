8e71ef66d23b53c8ff5ffc9b4840e21fa2bdd255
Eric <coralvanda@gmail.com>
2016-12-03 10:05:38 +0100

    refactor: Trim out unnecessary functions
    Remove the defineViewport and setVisibilityLists functions.  The code
    functions just the same when using the $sections variable in the
    controlFade function in place of the $sectionsOutViewport variable.
    Code still not functioning as ultimately desired, but this change
    should help make it easier to make the right changes and reach the
    desired final functionality.

--- portfolio/static/script.js
+++ portfolio/static/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// based on tympanus.net article on lateral scrolling by Mary Lou
+// based on a tympanus.net article on lateral scrolling by Mary Lou
 
 var $sections = $('section');
 var	$sectionsViewport, $sectionsOutViewport;
@@ -10,15 +10,7 @@ var	winSize	= {};
 var init = function() {
 	getWinSize();
 	initEvents();
-	defineViewport();
-	setVisibilityLists();
 	controlFade();
-};
-
-var setVisibilityLists = function() {
-	// builds lists of what is or isn't in view
-	$sectionsViewport 		= $sections.filter(':inviewport');
-	$sectionsOutViewport	= $sections.not( $sectionsViewport );
 };
 
 var getWinSize = function() {
@@ -28,46 +20,30 @@ var getWinSize = function() {
 
 var initEvents = function() {
 	// manages resize and scroll events, updating app as needed
-	$(window).on('resize', function( event ) {
+	$(window).resize(function( event ) {
 		// sets a new initial state based on the resize event
 		getWinSize();
-		setVisibilityLists();
+		//setVisibilityLists();
 	}),
 	$(window).scroll(function( event ) {
 		// adjust visibility after a scroll event
-		setVisibilityLists();
+		//setVisibilityLists();
 		controlFade();
-	});
-};
-
-var defineViewport = function() {
-	// defines the inviewport selector
-	$.extend( $.expr[':'], {
-		inviewport: function ( el ) {
-			var elTop = $(el).offset.top;
-			var elBottom = $(el).height() + elTop;
-			if (( elTop < winSize.height )
-				&& ( elBottom > $win.offset.top)) {
-				return true;
-			}
-			return false;
-		}
 	});
 };
 
 var controlFade = function() {
 	// adds or removes the "fade" class to control visibility as needed
 	var winscroll = $win.scrollTop();
-	$sectionsOutViewport.each( function(i) {
+	//$sectionsOutViewport.each( function(i) {
+	$sections.each(function(i) {
 		var $item = $(this);
 		var itemTop	= $item.offset().top;
 		var itemBottom = $item.height() + itemTop;
-		// hide the item if it is out of the viewport
 		if (( itemTop > winSize.height + winscroll )
 			|| ( itemBottom < $win.offset.top )) {
 			$item.removeClass("fade");
 		}
-		// if not, make it visible
 		else {
 			$item.addClass("fade");
 		}
