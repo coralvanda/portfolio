fa503ae1513e28e507a1fcacab7f56f794f1bf60
Eric <coralvanda@gmail.com>
2016-12-08 16:18:19 +0100

    feat: Add test-index.html page

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def front_page():
     return render_template("index.html")
 
+@app.route("/test")
+def test():
+	return render_template("test-index.html")
+
 @app.route("/kittyclicker")
 def kitty_clicker():
 	return render_template("catclicker.html")
